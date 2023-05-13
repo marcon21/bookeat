3892f458f016feaa78c9b240e93c3d93f45373e6
Carolina <carolina.sopranzetti@studenti.unitn.it>
2023-05-13 14:08:24 +0200

    menuRoute.jsx's page setup

--- frontend/src/routes/menuRoute.jsx
+++ frontend/src/routes/menuRoute.jsx
@@ -1,27 +1,34 @@
 export default function MenuRoute() {
     return (
-        <div className="row">
-            <div className="col-3 bg-dark h-100">
-                {/* Lateral Bar */}
-                <div className="container-fluid">
-                    <button type="button" className="btn btn-primary">Primary</button>
-                    <p>ciao</p>
-                </div>
-            </div>
-            <div className="col">
-                <div className="row">
-                    {/* Navbar */}
-                    <div className="container-fluid dark">
-                        <p>ciao</p>
+        <>
+            <div className="container-fluid">
+                <div className="row h-100">
+                    <div className="col col-md-2 bg-light">
+                        <div className="sticky">
+                            {/*Lateral Bar*/}
+                            <button type="button" className="btn btn-primary">Primary</button>
+                            <p>ciao</p>
+                        </div>
+                    </div>
+
+                    <div className="col col-md-10 bg-dark">
+                        <div className="row row-md-2 bg-info">
+                            <div className="sticky">
+                                {/*NavBar*/}
+                                <button type="button" className="btn btn-primary">Primary</button>
+                                <p>ciao</p>
+                            </div>
+                        </div>
+                        <div className="container-fluid row">
+                            <div className="sticky">
+                                {/*Menu Body*/}
+                                <button type="button" className="btn btn-primary">Primary</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="row">
-                    {/* Menu Body */}
-                    <div className="container-fluid dark">
-                        <p>ciao</p>
-                    </div>
-                </div>
             </div>
-        </div>
+        </>
+        
     )
 }
