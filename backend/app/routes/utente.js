605846910aac9d0d58ecae7b2bf81bfbbb8377ca
Carolina <carolina.sopranzetti@studenti.unitn.it>
2023-05-31 16:26:38 +0200

    implementation of route changepassword and changename

--- backend/app/routes/utente.js
+++ backend/app/routes/utente.js
@@ -13,4 +13,38 @@ router.get("/profile", async (req, res, next) => {
   successRes(res, "User profile", { user: user });
 });
 
+//Route for change password
+router.post("/profile/changepassword", async (req, res, next) => {
+  // Ottieni l'ID dell'utente loggato
+  const userId = req.user._id; 
+  // Ottieni vecchia e nuova password dalla richiesta
+  const { oldPassword, newPassword } = req.body; 
+
+  try {
+    // Chiamare il metodo per cambiare la password nel GestoreProfilo
+    await GestoreProfilo.modificaPassword(userId, oldPassword, newPassword);
+
+    successRes(res, "Password cambiata con successo");
+  } catch (error) {
+    errorRes(res, "Impossibile cambiare la password", error);
+  }
+});
+
+//Route for change name
+router.post("/profile/changename", async (req, res, next) => {
+  // Ottieni l'ID dell'utente loggato
+  const userId = req.user._id; 
+  // Ottieni nuovo nome dalla richiesta
+  const { Name } = req.body; 
+
+  try {
+    // Chiamare il metodo per cambiare la password nel GestoreProfilo
+    await GestoreProfilo.modificaNome(userId, Name);
+
+    successRes(res, "Nome cambiato con successo");
+  } catch (error) {
+    errorRes(res, "Impossibile cambiare il nome", error);
+  }
+});
+
 module.exports = router;
