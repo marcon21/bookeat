b56e9254120f22157e71cbc9096b8c5c52a03fdc
Jack <tezza.giacomo01@gmail.com>
2023-05-31 10:27:53 +0200

    integrated an error handling of the 304 response. momentary fix

--- frontend/src/requests.jsx
+++ frontend/src/requests.jsx
@@ -5,157 +5,113 @@ const backendUrl = 'http://localhost:3001/api/v1'
 //     'Authentication': 'Bearer '.concat(token)
 // },
 
-// API: GET /menu, get all plates in the menu
-// returns an array, first element is a boolean that indicate the success of the request, 
-// second element is the data or the error message
-export async function getMenu() {
+async function fetchAPI(endpoint, method, body = {}) {
     const requestOptions = {
-        method: 'GET',
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache'
+        }
     }
-    let r = await fetch(backendUrl.concat('/menu'), requestOptions)
+    if (method != 'GET') {
+        requestOptions["body"] = JSON.stringify(body)
+    }
+    let rt = await fetch(backendUrl.concat(endpoint), requestOptions)
         .then(res => res.json())
         .then(data => {
-            if (data['success']) {
+            if (data['success'] && JSON.stringify(data['data']) === JSON.stringify({})) {
+                return { "status": data['success'], "data": data['message'] }
+            } else if (data['success']) {
                 return { "status": data['success'], "data": data['data'] }
             } else {
                 return { "status": data['success'], "message": data['error'] }
             }
         })
-    return r
+    return (rt)
+}
+
+// API: GET /menu, get all plates in the menu
+// returns an array, first element is a boolean that indicate the success of the request, 
+// second element is the data or the error message
+export async function getMenu() {
+    return (await fetchAPI('/menu', 'GET'))
 }
 
 // API: POST /menu, insert a new plate,
 // returns an array, first element is a boolean that indicate the success of the request, 
 // second element is the data or the error message
 export async function insertPlate(name, price, category, available, description, allergenes, ingredients) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: {
-            "nome": name, // string
-            "prezzo": price, // int in cents (e.g. 1000 = 10€)
-            "categoria": category, // object of strings (e.g. {"primaria": "Antipasti", "secondaria": "Terra"})
-            "disponibile": available, // int to represent boolean (e.g. 1 = true, 0 = false)
-            "descrizione": description, // string
-            "allergeni": allergenes, //list of strings (e.g. ["Glutine", "Lattosio"])
-            "ingredientiModificabili": ingredients // list of strings (e.g. ["Pomodoro", "Mozzarella"])
-        }
-    }
-    let r = await fetch(backendUrl.concat('/menu'), requestOptions)
-        .then(res => res.json())
-        .then(data => {
-            if (data['success']) {
-                return { "status": data['success'], "data": data['data'] }
-            } else {
-                return { "status": data['success'], "message": data['error'] }
-            }
-        })
-    return r
+    return (await fetchAPI('/menu', 'POST', {
+        "nome": name, // string
+        "prezzo": price, // int in cents (e.g. 1000 = 10€)
+        "categoria": category, // object of strings (e.g. {"primaria": "Antipasti", "secondaria": "Terra"})
+        "disponibile": available, // int to represent boolean (e.g. 1 = true, 0 = false)
+        "descrizione": description, // string
+        "allergeni": allergenes, //list of strings (e.g. ["Glutine", "Lattosio"])
+        "ingredientiModificabili": ingredients // list of strings (e.g. ["Pomodoro", "Mozzarella"])
+    }))
 }
 
 // API: PUT /menu, edit a plate,
 // returns an array, first element is a boolean that indicate the success of the request, 
 // second element is the data or the error message
 export async function editPlate(plateID, name, price, category, available, description, allergenes, ingredients) {
-    const requestOptions = {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: {
-            "nome": name, // string
-            "prezzo": price, // int in cents (e.g. 1000 = 10€)
-            "categoria": category, // object of strings (e.g. {"primaria": "Antipasti", "secondaria": "Terra"})
-            "disponibile": available, // int to represent boolean (e.g. 1 = true, 0 = false)
-            "descrizione": description, // string
-            "allergeni": allergenes, //list of strings (e.g. ["Glutine", "Lattosio"])
-            "ingredientiModificabili": ingredients // list of strings (e.g. ["Pomodoro", "Mozzarella"])
-        }
-    }
-    let r = await fetch(backendUrl.concat('/menu/').concat(plateID), requestOptions)
-        .then(res => res.json())
-        .then(data => {
-            if (data['success']) {
-                return { "status": data['success'], "data": data['data'] }
-            } else {
-                return { "status": data['success'], "message": data['error'] }
-            }
-        })
-    return r
+    return (await fetchAPI('/menu/'.concat(plateID), 'PUT', {
+        "nome": name, // string
+        "prezzo": price, // int in cents (e.g. 1000 = 10€)
+        "categoria": category, // object of strings (e.g. {"primaria": "Antipasti", "secondaria": "Terra"})
+        "disponibile": available, // int to represent boolean (e.g. 1 = true, 0 = false)
+        "descrizione": description, // string
+        "allergeni": allergenes, //list of strings (e.g. ["Glutine", "Lattosio"])
+        "ingredientiModificabili": ingredients // list of strings (e.g. ["Pomodoro", "Mozzarella"])
+    }))
 }
 
 // API: DELETE /menu, delete a plate,
 // returns an array, first element is a boolean that indicate the success of the request, 
 // second element is the data or the error message
 export async function deletePlate(plateID) {
-    const requestOptions = {
-        method: 'DELETE',
-    }
-    let r = await fetch(backendUrl.concat('/menu/').concat(plateID), requestOptions)
-        .then(res => res.json())
-        .then(data => {
-            if (data['success']) {
-                return { "status": data['success'], "data": data['data'] }
-            } else {
-                return { "status": data['success'], "message": data['error'] }
-            }
-        })
-    return r
+    return (await fetchAPI('/menu/'.concat(plateID), 'DELETE'))
 }
 
 // API POST /auth/signUp, sign up a new user,
 // returns an array, first element is a boolean that indicate the success of the request, 
 // second element is the data or the error message
 export async function signUp(email, pw) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            "email": email,
-            "password": pw
-        })
-    }
-    let r = await fetch(backendUrl.concat('/auth/signup'), requestOptions)
-        .then(res => res.json())
-        .then(data => {
-            if (data['success']) {
-                let token = data['data']['token']
-                let userType = data['data']['userType']
-                document.cookie = "jwt=" + token + "; path=/; max-age=86400; samesite=lax"
-                document.cookie = "userType=" + userType + "; path=/; max-age=86400; samesite=lax"
-                return { "status": data['success'], "data": data['data'] }
-            } else {
-                return { "status": data['success'], "message": data['error'] }
-            }
-        })
-    return r
+    return (await fetchAPI('/auth/signup', 'POST', {
+        "email": email,
+        "password": pw
+    })).then(data => {
+        if (data['status']) {
+            let token = data['data']['token']
+            let userType = data['data']['userType']
+            document.cookie = "jwt=" + token + "; path=/; max-age=86400; samesite=lax"
+            document.cookie = "userType=" + userType + "; path=/; max-age=86400; samesite=lax"
+            return { "status": data['status'], "data": data['data'] }
+        } else {
+            return { "status": data['status'], "message": data['message'] }
+        }
+    })
 }
 
 // API POST /auth/login, login a user,
 // returns an array, first element is a boolean that indicate the success of the request, 
 // second element is the data or the error message
 export async function login(email, pw) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            "email": email,
-            "password": pw
-        })
-    }
-    let r = await fetch(backendUrl.concat('/auth/login'), requestOptions)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            if (data['success']) {
-                let token = data['data']['token']
-                let userType = data['data']['userType']
-                document.cookie = "jwt=" + token + "; path=/; max-age=86400; samesite=lax"
-                document.cookie = "userType=" + userType + "; path=/; max-age=86400; samesite=lax"
-                return { "status": data['success'], "data": data['data'] }
-            } else {
-                return { "status": data['success'], "message": data['error'] }
-            }
-        })
-    return r
+    return (await fetchAPI('/auth/login', 'POST', {
+        "email": email,
+        "password": pw
+    })).then(data => {
+        if (data['status']) {
+            let token = data['data']['token']
+            let userType = data['data']['userType']
+            document.cookie = "jwt=" + token + "; path=/; max-age=86400; samesite=lax"
+            document.cookie = "userType=" + userType + "; path=/; max-age=86400; samesite=lax"
+            return { "status": data['status'], "data": data['data'] }
+        } else {
+            return { "status": data['status'], "message": data['message'] }
+        }
+    })
 }
 
 // Frontend only - logout a user dropping the cookie
