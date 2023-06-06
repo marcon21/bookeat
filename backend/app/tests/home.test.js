3fc6254ce0d8cf477ac0dffe62cb271d11aa7c76
Jack <tezza.giacomo01@gmail.com>
2023-06-06 14:32:41 +0200

    implemented tests for home APIs

---
+++ backend/app/tests/home.test.js
@@ -0,0 +1,11 @@
+const fetchAPI = require('./utils').fetchAPI
+
+describe('Home', () => {
+    describe('GET /', () => {
+        it('should return 200', async () => {
+            const res = await fetchAPI('/', 'GET')
+            expect(res.statusCode).toEqual(200)
+        })
+    })
+}
+)
