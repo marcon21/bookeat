a27158ede209d416500a67e5dac11b2d594e9af8
Carolina <carolina.sopranzetti@studenti.unitn.it>
2023-06-06 12:10:16 +0200

    link setting page to button and accessible only if LoggedIn

--- frontend/src/routes/settingsRoute.jsx
+++ frontend/src/routes/settingsRoute.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { Link, Navigate } from "react-router-dom"
 import { userInfo } from '../requests'
 import * as yup from "yup";
 
@@ -11,7 +12,11 @@ export async function loader() {
     return user
 }
 
-export default function SettingsRoute(setRedirect) {
+export default function SettingsRoute() {
+    const isLoggedIn = document.cookie.split(';').some((item) => item.trim().startsWith('jwt='))
+
+    const [redirect, setRedirect] = useState(!isLoggedIn ? '/login' : false)
+
     let schema = yup.object().shape({
         usernome: yup.string()
             .required("Username obbligatorio")
@@ -50,89 +55,99 @@ export default function SettingsRoute(setRedirect) {
         // Handle form submission
     };
 
-    
-    
-
-    if (isEditing) {
-        return (
-            <>
-                <section className="vh-100 bg-dark">
-                    <div className="container py-5 h-100 ">
-                        <div className="row d-flex justify-content-center align-items-center h-100">
-                            <div className="col col-xl-10">
-                                <a className="nav-link px-3" role="button" onClick={() => setIsEditing(false)}>
-                                    <i className='bi bi-arrow-left-circle-fill fs-4 bg-light'></i>
-                                </a>
-                                <div className="card" style={{ borderRadius: "1rem" }}>
-                                    <div className="row g-0">
-                                        <div className="col-md-6 col-lg-7 d-flex align-items-center">
-                                            <div className="card-body p-4 p-lg-5 text-black">
-                                                <h2>Modifica i tuoi Dati:</h2>
-                                                <form onSubmit={handleSubmit(onSubmit)}>
-                                                    <div className="row">
-                                                        <label htmlFor="usernome">Username</label>
-                                                        <input type="text" className="form-control" id="nome" {...register('usernome')} />
-                                                        <p className="text-danger">{errors.usernome?.message}</p>
-                                                    </div>
-                                                    <div className="row">
-                                                        <label htmlFor="email">Email</label>
-                                                        <input type="text" className="form-control" id="nome" {...register('email')} />
-                                                        <p className="text-danger">{errors.email?.message}</p>
-                                                    </div>
-                                                    <div className="row">
-                                                        <label htmlFor="password">Nuova Password</label>
-                                                        <input type="password" className="form-control" id="nome" {...register('password')} />
-                                                        <p className="text-danger">{errors.password?.message}</p>
-                                                    </div>
-                                                    <div className="row">
-                                                        <label htmlFor="cpassword">Conferma Password</label>
-                                                        <input type="password" className="form-control" id="nome" {...register('cpassword')} />
-                                                        <p className="text-danger">{errors.cpassword?.message}</p>
-                                                    </div>
-                                                    <button type="submit">Conferma modifica</button>
-                                                </form>
+    if (isLoggedIn) {
+        if (isEditing) {
+            return (
+                <>
+                    <section className="vh-100 bg-dark">
+                        <div className="container py-5 h-100 ">
+                            <div className="row d-flex justify-content-center align-items-center h-100">
+                                <div className="col col-xl-10">
+                                    <a className="nav-link px-3" role="button" onClick={() => setIsEditing(false)}>
+                                        <i className='bi bi-arrow-left-circle-fill fs-4 bg-light'></i>
+                                    </a>
+                                    <div className="card" style={{ borderRadius: "1rem" }}>
+                                        <div className="row g-0">
+                                            <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                                                <div className="card-body p-4 p-lg-5 text-black">
+                                                    <h2>Modifica i tuoi Dati:</h2>
+                                                    <form onSubmit={handleSubmit(onSubmit)}>
+                                                        <div className="row">
+                                                            <label htmlFor="usernome">Username</label>
+                                                            <input type="text" className="form-control" id="nome" {...register('usernome')} />
+                                                            <p className="text-danger">{errors.usernome?.message}</p>
+                                                        </div>
+                                                        <div className="row">
+                                                            <label htmlFor="email">Email</label>
+                                                            <input type="text" className="form-control" id="nome" {...register('email')} />
+                                                            <p className="text-danger">{errors.email?.message}</p>
+                                                        </div>
+                                                        <div className="row">
+                                                            <label htmlFor="password">Nuova Password</label>
+                                                            <input type="password" className="form-control" id="nome" {...register('password')} />
+                                                            <p className="text-danger">{errors.password?.message}</p>
+                                                        </div>
+                                                        <div className="row">
+                                                            <label htmlFor="cpassword">Conferma Password</label>
+                                                            <input type="password" className="form-control" id="nome" {...register('cpassword')} />
+                                                            <p className="text-danger">{errors.cpassword?.message}</p>
+                                                        </div>
+                                                        <button type="submit">Conferma modifica</button>
+                                                    </form>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </section>
-            </>
-        )
+                    </section>
+                </>
+            )
+        } else {
+            return (
+                //dati solo visulizzazione
+                <>
+                {redirect && <Navigate to={redirect} />}
+                    <section className="vh-100 bg-dark">
+                        <div className="container py-5 h-100">
+                            <div className="row d-flex justify-content-center align-items-center h-100">
+                                <div className="col col-xl-10">
+                                    <a className="nav-link px-3" role="button" onClick={() => {
+                                        setRedirect('/')
+                                    }}>
+                                        <i className='bi bi-arrow-left-circle-fill fs-4 bg-light'></i>
+                                    </a>
+                                    <div className="card" style={{ borderRadius: "1rem" }}>
+                                        <div className="row g-0">
+                                            <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                                                <div className="card-body p-4 p-lg-5 text-black">
+                                                    <h2>I tuoi Dati:</h2>
+                                                    <div className="row">
+                                                        <p className="font-label">Username:</p>
+                                                        <p>{userData.usernome}</p>
+                                                    </div>
+                                                    <div className="row">
+                                                        <p className="font-label">Email:</p>
+                                                        <p>{userData.email}</p>
+                                                    </div>
+                                                    <button onClick={() => setIsEditing(true)}>Modifica</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section >
+                </>
+            )
+        }
     } else {
         return (
-            //dati solo visulizzazione
-            <section className="vh-100 bg-dark">
-                <div className="container py-5 h-100">
-                    <div className="row d-flex justify-content-center align-items-center h-100">
-                        <div className="col col-xl-10">
-                            <a className="nav-link px-3" role="button" onClick={() => {    }}>
-                                <i className='bi bi-arrow-left-circle-fill fs-4 bg-light'></i>
-                            </a>
-                            <div className="card" style={{ borderRadius: "1rem" }}>
-                                <div className="row g-0">
-                                    <div className="col-md-6 col-lg-7 d-flex align-items-center">
-                                        <div className="card-body p-4 p-lg-5 text-black">
-                                            <h2>I tuoi Dati:</h2>
-                                            <div className="row">
-                                                <p className="font-label">Username:</p>
-                                                <p>{userData.usernome}</p>
-                                            </div>
-                                            <div className="row">
-                                                <p className="font-label">Email:</p>
-                                                <p>{userData.email}</p>
-                                            </div>
-                                            <button onClick={() => setIsEditing(true)}>Modifica</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section >
+            <>
+                {redirect && <Navigate to={redirect} />}
+            </>
         )
     }
 }
