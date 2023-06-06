bd7153b3ca56d3a94b9e3f4e3accccdd705be4b2
Jack <tezza.giacomo01@gmail.com>
2023-06-06 15:32:37 +0200

    implemented tests for menu APIs

---
+++ backend/app/tests/menu.test.js
@@ -0,0 +1,320 @@
+const fetchAPI = require('./utils').fetchAPI
+const makeString = require('./utils').makeString
+const makeNumber = require('./utils').makeNumber
+
+// importa generaJWT da GestoreProfilo.js
+const generaJWT = require('../gestori/GestoreProfilo').generaJWT
+const getMenu = require('../gestori/GestoreMenu').getMenu
+const User = require("../db/utente").User
+
+
+const { get } = require('mongoose')
+const piatti = require('../utils/piatti.json')
+const utenti = require('../utils/utenti.json')
+const utenteManager = utenti.find((utente) => utente.userType === 'Manager')
+
+describe('Menu', () => {
+
+    describe('GET /menu', () => {
+        it('should return 200', async () => {
+            const res = await fetchAPI('/menu', 'GET')
+            expect(res.statusCode).toEqual(200)
+        })
+    })
+    describe('POST /menu', () => {
+        it('should return 401 if user is not logged in', async () => {
+            const res = await fetchAPI('/menu', 'POST', {
+                nome: piatti[0].nome,
+                prezzo: piatti[0].prezzo,
+                categoria: piatti[0].categoria,
+                disponibile: piatti[0].disponibile,
+                descrizione: piatti[0].descrizione,
+                allergeni: piatti[0].allergeni,
+                ingredientiModificabili: piatti[0].ingredientiModificabili,
+            })
+            expect(res.statusCode).toEqual(401)
+        })
+        it('should return 401 if user is not a manager', async () => {
+            const utenteManagerId = await User.findOne({ email: utenteManager.email }).then((res) => res._id)
+            const token = generaJWT(utenteManagerId, utenteManager.email)
+            const res = await fetchAPI('/menu', 'POST', {
+                nome: piatti[0].nome,
+                prezzo: piatti[0].prezzo,
+                categoria: piatti[0].categoria,
+                disponibile: piatti[0].disponibile,
+                descrizione: piatti[0].descrizione,
+                allergeni: piatti[0].allergeni,
+                ingredientiModificabili: piatti[0].ingredientiModificabili,
+            }, makeString(token.length))
+            expect(res.statusCode).toEqual(401)
+        })
+        it('should return 424 if name is not valid', async () => {
+            const utenteManagerId = await User.findOne({ email: utenteManager.email }).then((res) => res._id)
+            const token = generaJWT(utenteManagerId, utenteManager.email)
+            const res = await fetchAPI('/menu', 'POST', {
+                nome: '',
+                prezzo: piatti[0].prezzo,
+                categoria: piatti[0].categoria,
+                disponibile: piatti[0].disponibile,
+                descrizione: piatti[0].descrizione,
+                allergeni: piatti[0].allergeni,
+                ingredientiModificabili: piatti[0].ingredientiModificabili,
+            }, token)
+            expect(res.statusCode).toEqual(424)
+        })
+        it('should return 424 if price is not valid', async () => {
+            const utenteManagerId = await User.findOne({ email: utenteManager.email }).then((res) => res._id)
+            const token = generaJWT(utenteManagerId, utenteManager.email)
+            const res = await fetchAPI('/menu', 'POST', {
+                nome: piatti[0].nome,
+                prezzo: makeString(10),
+                categoria: piatti[0].categoria,
+                disponibile: piatti[0].disponibile,
+                descrizione: piatti[0].descrizione,
+                allergeni: piatti[0].allergeni,
+                ingredientiModificabili: piatti[0].ingredientiModificabili,
+            }, token)
+            expect(res.statusCode).toEqual(424)
+        })
+        it('should return 424 if category is not valid', async () => {
+            const utenteManagerId = await User.findOne({ email: utenteManager.email }).then((res) => res._id)
+            const token = generaJWT(utenteManagerId, utenteManager.email)
+            const res = await fetchAPI('/menu', 'POST', {
+                nome: piatti[0].nome,
+                prezzo: piatti[0].prezzo,
+                categoria: makeString(10),
+                disponibile: piatti[0].disponibile,
+                descrizione: piatti[0].descrizione,
+                allergeni: piatti[0].allergeni,
+                ingredientiModificabili: piatti[0].ingredientiModificabili,
+            }, token)
+            expect(res.statusCode).toEqual(424)
+        })
+        it('should return 424 if available is not valid', async () => {
+            const utenteManagerId = await User.findOne({ email: utenteManager.email }).then((res) => res._id)
+            const token = generaJWT(utenteManagerId, utenteManager.email)
+            const res = await fetchAPI('/menu', 'POST', {
+                nome: piatti[0].nome,
+                prezzo: piatti[0].prezzo,
+                categoria: piatti[0].categoria,
+                disponibile: makeString(10),
+                descrizione: piatti[0].descrizione,
+                allergeni: piatti[0].allergeni,
+                ingredientiModificabili: piatti[0].ingredientiModificabili,
+            }, token)
+            expect(res.statusCode).toEqual(424)
+        })
+        it('should return 424 if description is not valid', async () => {
+            const utenteManagerId = await User.findOne({ email: utenteManager.email }).then((res) => res._id)
+            const token = generaJWT(utenteManagerId, utenteManager.email)
+            const res = await fetchAPI('/menu', 'POST', {
+                nome: piatti[0].nome,
+                prezzo: piatti[0].prezzo,
+                categoria: piatti[0].categoria,
+                disponibile: piatti[0].disponibile,
+                descrizione: '',
+                allergeni: piatti[0].allergeni,
+                ingredientiModificabili: piatti[0].ingredientiModificabili,
+            }, token)
+            expect(res.statusCode).toEqual(424)
+        })
+        it('should return 424 if allergens is not valid', async () => {
+            const utenteManagerId = await User.findOne({ email: utenteManager.email }).then((res) => res._id)
+            const token = generaJWT(utenteManagerId, utenteManager.email)
+            const res = await fetchAPI('/menu', 'POST', {
+                nome: piatti[0].nome,
+                prezzo: piatti[0].prezzo,
+                categoria: piatti[0].categoria,
+                disponibile: piatti[0].disponibile,
+                descrizione: piatti[0].descrizione,
+                allergeni: makeString(10),
+                ingredientiModificabili: piatti[0].ingredientiModificabili,
+            }, token)
+            expect(res.statusCode).toEqual(424)
+        })
+        it('should return 424 if modifiable ingredients is not valid', async () => {
+            const utenteManagerId = await User.findOne({ email: utenteManager.email }).then((res) => res._id)
+            const token = generaJWT(utenteManagerId, utenteManager.email)
+            const res = await fetchAPI('/menu', 'POST', {
+                nome: piatti[0].nome,
+                prezzo: piatti[0].prezzo,
+                categoria: piatti[0].categoria,
+                disponibile: piatti[0].disponibile,
+                descrizione: piatti[0].descrizione,
+                allergeni: piatti[0].allergeni,
+                ingredientiModificabili: makeString(10),
+            }, token)
+            expect(res.statusCode).toEqual(424)
+        })
+        it('should return 201 if all fields are valid', async () => {
+            const utenteManagerId = await User.findOne({ email: utenteManager.email }).then((res) => res._id)
+            const token = generaJWT(utenteManagerId, utenteManager.email)
+            const res = await fetchAPI('/menu', 'POST', {
+                nome: makeString(10),
+                prezzo: makeNumber(4),
+                categoria: {
+                    "primaria": makeString(10),
+                    "secondaria": makeString(10)
+                },
+                disponibile: true,
+                descrizione: makeString(10),
+                allergeni: [makeString(10), makeString(10)],
+                ingredientiModificabili: [makeString(10), makeString(10)]
+            }, token)
+            expect(res.statusCode).toEqual(201)
+        })
+    })
+    describe('PUT /menu/:idPiatto', () => {
+        it('should return 401 if user is not logged in', async () => {
+            const idPiatto = await getMenu().then((r) => r.piatti[0]._id)
+            const res = await fetchAPI(`/menu/${idPiatto}`, 'PUT', {
+                nome: piatti[0].nome,
+                prezzo: piatti[0].prezzo,
+                categoria: piatti[0].categoria,
+                disponibile: piatti[0].disponibile,
+                descrizione: piatti[0].descrizione,
+                allergeni: piatti[0].allergeni,
+                ingredientiModificabili: piatti[0].ingredientiModificabili
+            })
+            expect(res.statusCode).toEqual(401)
+        })
+        it('should return 401 if user is not a manager', async () => {
+            const utenteManagerId = await User.findOne({ email: utenteManager.email }).then((res) => res._id)
+            const token = generaJWT(utenteManagerId, utenteManager.email)
+            const idPiatto = await getMenu().then((r) => r.piatti[0]._id)
+            const res = await fetchAPI(`/menu/${idPiatto}`, 'PUT', {
+                nome: piatti[0].nome,
+                prezzo: piatti[0].prezzo,
+                categoria: piatti[0].categoria,
+                disponibile: piatti[0].disponibile,
+                descrizione: piatti[0].descrizione,
+                allergeni: piatti[0].allergeni,
+                ingredientiModificabili: piatti[0].ingredientiModificabili
+            }, makeString(token.length))
+            expect(res.statusCode).toEqual(401)
+        })
+        it('should return 424 if name is not valid', async () => {
+            const utenteManagerId = await User.findOne({ email: utenteManager.email }).then((res) => res._id)
+            const token = generaJWT(utenteManagerId, utenteManager.email)
+            const idPiatto = await getMenu().then((r) => r.piatti[0]._id)
+            const res = await fetchAPI(`/menu/${idPiatto}`, 'PUT', {
+                nome: '',
+                prezzo: piatti[0].prezzo,
+                categoria: piatti[0].categoria,
+                disponibile: piatti[0].disponibile,
+                descrizione: piatti[0].descrizione,
+                allergeni: piatti[0].allergeni,
+                ingredientiModificabili: piatti[0].ingredientiModificabili
+            }, token)
+            expect(res.statusCode).toEqual(424)
+        })
+        it('should return 424 if price is not valid', async () => {
+            const utenteManagerId = await User.findOne({ email: utenteManager.email }).then((res) => res._id)
+            const token = generaJWT(utenteManagerId, utenteManager.email)
+            const idPiatto = await getMenu().then((r) => r.piatti[0]._id)
+            const res = await fetchAPI(`/menu/${idPiatto}`, 'PUT', {
+                nome: piatti[0].nome,
+                prezzo: makeString(10),
+                categoria: piatti[0].categoria,
+                disponibile: piatti[0].disponibile,
+                descrizione: piatti[0].descrizione,
+                allergeni: piatti[0].allergeni,
+                ingredientiModificabili: piatti[0].ingredientiModificabili
+            }, token)
+            expect(res.statusCode).toEqual(424)
+        })
+        it('should return 424 if category is not valid', async () => {
+            const utenteManagerId = await User.findOne({ email: utenteManager.email }).then((res) => res._id)
+            const token = generaJWT(utenteManagerId, utenteManager.email)
+            const idPiatto = await getMenu().then((r) => r.piatti[0]._id)
+            const res = await fetchAPI(`/menu/${idPiatto}`, 'PUT', {
+                nome: piatti[0].nome,
+                prezzo: piatti[0].prezzo,
+                categoria: makeString(10),
+                disponibile: piatti[0].disponibile,
+                descrizione: piatti[0].descrizione,
+                allergeni: piatti[0].allergeni,
+                ingredientiModificabili: piatti[0].ingredientiModificabili
+            }, token)
+            expect(res.statusCode).toEqual(424)
+        })
+        it('should return 424 if available is not valid', async () => {
+            const utenteManagerId = await User.findOne({ email: utenteManager.email }).then((res) => res._id)
+            const token = generaJWT(utenteManagerId, utenteManager.email)
+            const idPiatto = await getMenu().then((r) => r.piatti[0]._id)
+            const res = await fetchAPI(`/menu/${idPiatto}`, 'PUT', {
+                nome: piatti[0].nome,
+                prezzo: piatti[0].prezzo,
+                categoria: piatti[0].categoria,
+                disponibile: makeString(10),
+                descrizione: piatti[0].descrizione,
+                allergeni: piatti[0].allergeni,
+                ingredientiModificabili: piatti[0].ingredientiModificabili
+            }, token)
+            expect(res.statusCode).toEqual(424)
+        })
+        it('should return 424 if description is not valid', async () => {
+            const utenteManagerId = await User.findOne({ email: utenteManager.email }).then((res) => res._id)
+            const token = generaJWT(utenteManagerId, utenteManager.email)
+            const idPiatto = await getMenu().then((r) => r.piatti[0]._id)
+            const res = await fetchAPI(`/menu/${idPiatto}`, 'PUT', {
+                nome: piatti[0].nome,
+                prezzo: piatti[0].prezzo,
+                categoria: piatti[0].categoria,
+                disponibile: piatti[0].disponibile,
+                descrizione: '',
+                allergeni: piatti[0].allergeni,
+                ingredientiModificabili: piatti[0].ingredientiModificabili
+            }, token)
+            expect(res.statusCode).toEqual(424)
+        })
+        it('should return 424 if allergens is not valid', async () => {
+            const utenteManagerId = await User.findOne({ email: utenteManager.email }).then((res) => res._id)
+            const token = generaJWT(utenteManagerId, utenteManager.email)
+            const idPiatto = await getMenu().then((r) => r.piatti[0]._id)
+            const res = await fetchAPI(`/menu/${idPiatto}`, 'PUT', {
+                nome: piatti[0].nome,
+                prezzo: piatti[0].prezzo,
+                categoria: piatti[0].categoria,
+                disponibile: piatti[0].disponibile,
+                descrizione: piatti[0].descrizione,
+                allergeni: makeString(10),
+                ingredientiModificabili: piatti[0].ingredientiModificabili
+            }, token)
+            expect(res.statusCode).toEqual(424)
+        })
+        it('should return 424 if modifiable ingredients is not valid', async () => {
+            const utenteManagerId = await User.findOne({ email: utenteManager.email }).then((res) => res._id)
+            const token = generaJWT(utenteManagerId, utenteManager.email)
+            const idPiatto = await getMenu().then((r) => r.piatti[0]._id)
+            const res = await fetchAPI(`/menu/${idPiatto}`, 'PUT', {
+                nome: piatti[0].nome,
+                prezzo: piatti[0].prezzo,
+                categoria: piatti[0].categoria,
+                disponibile: piatti[0].disponibile,
+                descrizione: piatti[0].descrizione,
+                allergeni: piatti[0].allergeni,
+                ingredientiModificabili: makeString(10)
+            }, token)
+            expect(res.statusCode).toEqual(424)
+        })
+        it('should return 200 if all fields are valid', async () => {
+            const utenteManagerId = await User.findOne({ email: utenteManager.email }).then((res) => res._id)
+            const token = generaJWT(utenteManagerId, utenteManager.email)
+            const idPiatto = await getMenu().then((r) => r.piatti[0]._id)
+            const res = await fetchAPI(`/menu/${idPiatto}`, 'PUT', {
+                nome: makeString(10),
+                prezzo: makeNumber(4),
+                categoria: {
+                    "primaria": makeString(10),
+                    "secondaria": makeString(10)
+                },
+                disponibile: true,
+                descrizione: makeString(10),
+                allergeni: [makeString(10), makeString(10)],
+                ingredientiModificabili: [makeString(10), makeString(10)]
+            }, token)
+            expect(res.statusCode).toEqual(200)
+        })
+    })
+})
--- backend/app/tests/utils.js
+++ backend/app/tests/utils.js
@@ -12,6 +12,18 @@ function makeString(length) {
     return result;
 }
 
+function makeNumber(length) {
+    let result = '';
+    const characters = '0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return result;
+}
+
 function makeEmail() {
     return makeString(10) + '@gmail.com';
 }
@@ -33,11 +45,16 @@ function makePassword() {
     }
 }
 
-async function fetchAPI(endpoint, method, body = {}) {
-    return (await request('http://localhost:3000/api/v1')[method.toLowerCase()](endpoint).send(body))
+async function fetchAPI(endpoint, method, body = {}, token = null) {
+    if (token) {
+        return (await request('http://localhost:3000/api/v1')[method.toLowerCase()](endpoint).send(body).set('Cookie', [`jwt=${token}`]).set('Access-Control-Allow-Credentials', 'true'))
+    } else {
+        return (await request('http://localhost:3000/api/v1')[method.toLowerCase()](endpoint).send(body))
+    }
 }
 
 exports.makeString = makeString
 exports.makeEmail = makeEmail
 exports.makePassword = makePassword
+exports.makeNumber = makeNumber
 exports.fetchAPI = fetchAPI
