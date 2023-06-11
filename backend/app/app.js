fb9bd0d28dc32e3b3c53fde5f79cddc19c176730
Jack <tezza.giacomo01@gmail.com>
2023-06-11 13:38:48 +0200

    allow all cors

--- backend/app/app.js
+++ backend/app/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(session(sess));
 app.use(
   cors({
-    origin: "http://localhost:4000",
+    origin: (origin, callback) => callback(null, true),
     credentials: true,
   })
 );
